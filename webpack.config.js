2fa4c9766de10e5d221a79318a8a75854582ca4c
nataliasabadysh <nataliasabadysh@gmail.com>
2018-10-30 14:33:00 +0400

    add css on mob

--- webpack.config.js
+++ webpack.config.js
@@ -4,82 +4,82 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: {
-    main: './src/index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: '',
-  },
-  module: {
-    rules: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: [
-          'style-loader',
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: "postcss-loader",
-            options: {
-              ident: "postcss",
-              plugins: (loader) => [
-                require('autoprefixer')({
-                  browsers: ['defaults']
-                })
-              ]
-            }
-          },
-          // 'postcss-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        exclude: /node_modules/,
-        use: [{
-            loader: 'url-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'img',
-              limit: 10000,
+    entry: {
+        main: './src/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '',
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: 'babel-loader',
+        },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            ident: "postcss",
+                            plugins: (loader) => [
+                                require('autoprefixer')({
+                                    browsers: ['defaults']
+                                })
+                            ]
+                        }
+                    },
+                    // 'postcss-loader',
+                    'sass-loader',
+                ],
             },
-          },
-          'img-loader',
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'img',
+                        limit: 10000,
+                    },
+                },
+                    'img-loader',
+                ],
+            },
+            {
+                test: /\.hbs$/,
+                exclude: /node_modules/,
+                use: 'handlebars-loader',
+            },
         ],
-      },
-      {
-        test: /\.hbs$/,
-        exclude: /node_modules/,
-        use: 'handlebars-loader',
-      },
+    },
+    plugins: [
+        new CleanWebpackPlugin('build'),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            hash: true,
+            template: './src/index.html',
+        }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin('build'),
-    new MiniCssExtractPlugin({
-      filename: 'styles.css',
-    }),
-    new HtmlWebpackPlugin({
-      inject: true,
-      hash: true,
-      template: './src/index.html',
-    }),
-  ],
-  devServer: {
-    publicPath: '/',
-    historyApiFallback: true,
-    noInfo: false,
-    quiet: false,
-    stats: 'errors-only',
-    clientLogLevel: 'warning',
-    compress: true,
-    port: 9001,
-  },
+    devServer: {
+        publicPath: '/',
+        historyApiFallback: true,
+        noInfo: false,
+        quiet: false,
+        stats: 'errors-only',
+        clientLogLevel: 'warning',
+        compress: true,
+        port: 9000,
+    },
 };
