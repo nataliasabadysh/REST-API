e0f1ba08f801104952b90e1401a07f15c98b32f1
nataliasabadysh <nataliasabadysh@gmail.com>
2018-10-30 20:01:09 +0400

    mob

--- src/js/model.js
+++ src/js/model.js
@@ -2,46 +2,46 @@ import * as modal from './services/modal'
 
 export default class Model {
 
-	constructor() {
-		this.currentQuery = '';
-		this.currentPage = 1;
-		this.localImages = JSON.parse(localStorage.getItem('images'));
+    constructor() {
+        this.currentQuery = '';
+        this.currentPage = 1;
+        this.localImages = JSON.parse(localStorage.getItem('images'));
 
-		if (this.localImages === null || this.localImages.length === 0) {
-			this.localImages = []
-		}
-	}
+        if (this.localImages === null || this.localImages.length === 0) {
+            this.localImages = []
+        }
+    }
 
-	resetCurrentPage() {
-		this.currentPage = 1;
-	}
+    resetCurrentPage() {
+        this.currentPage = 1;
+    }
 
-	incrementCurrentPage() {
-		this.currentPage += 1;
-	}
+    incrementCurrentPage() {
+        this.currentPage += 1;
+    }
 
-	addToLocalStorage(arr) {
-		const jsonObj = JSON.stringify(arr);
-		localStorage.setItem(`images`, jsonObj);
-	}
+    addToLocalStorage(arr) {
+        const jsonObj = JSON.stringify(arr);
+        localStorage.setItem(`images`, jsonObj);
+    }
 
-	isHasId(id, arr) {
-		return arr.some(obj => (obj.id === id))
-	}
+    isHasId(id, arr) {
+        return arr.some(obj => (obj.id === id))
+    }
 
-	backdropImageInit(selectedImage) {
-		return modal.showCurrentImage(selectedImage);
-	}
+    backdropImageInit(selectedImage) {
+        return modal.showCurrentImage(selectedImage);
+    }
 
-	backdropShowNextImage() {
-		return modal.showNextImage();
-	}
+    backdropShowNextImage() {
+        return modal.showNextImage();
+    }
 
-	backdropShowPrevImage() {
-		return modal.showPrevImage();
-	}
+    backdropShowPrevImage() {
+        return modal.showPrevImage();
+    }
 
-	backdropCloseModal() {
-		return modal.closeModal();
-	}
+    backdropCloseModal() {
+        return modal.closeModal();
+    }
 }
